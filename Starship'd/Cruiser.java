394f76797de8ab3d571723d664411082a6846338
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2019-04-01 08:32:09 -0700

    Update Cruiser.java

--- Starship'd/Cruiser.java
+++ Starship'd/Cruiser.java
@@ -1,1 +1,19 @@
+import java.util.Random;
+public class Cruiser extends Starship implements Offense{
 
+    public Cruiser(String newName){
+
+        name = newName;
+
+        Random rand = new Random();
+
+        hull = 2000 + (rand.nextInt(500)+1);
+        shield = 2000 + (rand.nextInt(500)+1);
+        beam = 3000 + (rand.nextInt(1000)+1);
+        torp = 10 + (rand.nextInt(15)+1);
+
+        System.out.printf("%s:      Hull: %d    Shields: %d    Beam Weapons: %d    Torpedoes: %d", newName,hull,shield,beam,torp);
+        System.out.println();
+
+    }
+}
