c4662bd94dc9bccdb7668901adcc64d843786b38
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2019-04-01 08:31:37 -0700

    Update Battleship.java

--- Starship'd/Battleship.java
+++ Starship'd/Battleship.java
@@ -1,1 +1,19 @@
+import java.util.Random;
+public class Battleship extends Starship implements Offense{
 
+    public Battleship(String newName){
+
+        name = newName;
+        Random rand = new Random();
+
+        hull = 3000 + (rand.nextInt(2000)+1);
+        shield = 1500 + (rand.nextInt(500)+1);
+        beam = 4000 + (rand.nextInt(1000)+1);
+        torp = 40 + (rand.nextInt(10)+1);
+
+        System.out.printf("%s:\tHull: %d\tShields: %d\tBeam Weapons: %d\tTorpedoes: %d", newName,hull,shield,beam,torp);
+        System.out.println();
+
+    }
+
+}
