ec57b5527aabd1a5de3cb6066a4be5bcbeb9e5a9
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2019-04-01 08:30:18 -0700

    Update Starship.java

--- Starship'd/Starship.java
+++ Starship'd/Starship.java
@@ -1,1 +1,11 @@
+public class Starship {
 
+    protected String name;
+    protected int hull;
+    protected int shield;
+    protected int beam;
+    protected int torp;
+    public static  void Starship(){
+
+    }
+}
